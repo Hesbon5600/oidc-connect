2b3b3bba2465728eb46a7c99192a2e2ba21d2b75
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2022-12-13 13:49:27 +0300

    code format

--- oidc_app/settings.py
+++ oidc_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1a%g71=ktu+&ju2+bh23xec!@82&xftq0(nc3$nq%f%60c0ir@"
+SECRET_KEY = os.environ.get("SECRET_KEY", "very-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
